ce95f9e233e6d522460b537f62e226212dc8d52a
Mr. Benson <99112362+GasComIT@users.noreply.github.com>
2023-04-23 18:57:52 +0300

    Update CODE.sh

--- CODE.sh
+++ CODE.sh
@@ -5,14 +5,9 @@ apt install nodejs --fix-missing -qq -yy
 clear
 pkg install git -qq -y
 clear
-npm install axios
-clear
-npm install chalk@4.1.2
-clear
-npm install
-clear
 git clone https://github.com/GasComIT/Vlkyre-QR
 clear
 cd Vlkyre-QR
+npm install
 clear
 node index.js
